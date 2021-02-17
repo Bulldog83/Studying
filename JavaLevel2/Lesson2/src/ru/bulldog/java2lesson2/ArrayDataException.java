4c1d0cbcfdfa47c6c83a68c677d23e407521c16f
Aleksey <bulldog.seregin@gmail.com>
2021-02-17 22:04:19 +0300

    Revert "Homework Lesson 2"

--- JavaLevel2/Lesson2/src/ru/bulldog/java2lesson2/ArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package ru.bulldog.java2lesson2;
-
-public class ArrayDataException extends RuntimeException {
-
-	public ArrayDataException(int row, int col) {
-		super(String.format("Wrong array element at: [%d][%d]", row, col));
-	}
-}
