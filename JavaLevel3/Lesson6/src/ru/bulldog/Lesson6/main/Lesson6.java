3a687b4f1269978c2d90fcf18264780203b73029
Aleksey <bulldog.seregin@gmail.com>
2021-04-07 15:09:41 +0300

    Homework Lesson 6

---
+++ JavaLevel3/Lesson6/src/ru/bulldog/Lesson6/main/Lesson6.java
@@ -0,0 +1,49 @@
+package ru.bulldog.Lesson6.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lesson6 {
+
+	public static int[] getAllAfterLast4(int... array) throws RuntimeException {
+		if (array.length == 0) {
+			throw new RuntimeException("Input can't be empty");
+		}
+		List<Integer> arrayList = fromArray(array);
+		int idx = arrayList.lastIndexOf(4);
+		if (idx == -1) {
+			throw new RuntimeException("There is no 4 in the array.");
+		}
+		return toArray(arrayList.subList(idx + 1, array.length));
+	}
+
+	public static boolean validate1And4(int... array) {
+		if (array.length == 0) return false;
+		int one = 0, four = 0;
+		for (int val : array) {
+			if (val == 1 || val == 4) {
+				if (val == 1) one++;
+				else four++;
+			} else {
+				return false;
+			}
+		}
+		return one > 0 && four > 0;
+	}
+
+	private static List<Integer> fromArray(int[] array) {
+		List<Integer> arrayList = new ArrayList<>();
+		for (int val : array) {
+			arrayList.add(val);
+		}
+		return arrayList;
+	}
+
+	private static int[] toArray(List<Integer> list) {
+		int[] array = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			array[i] = list.get(i);
+		}
+		return array;
+	}
+}
---
+++ JavaLevel3/Lesson6/src/ru/bulldog/Lesson6/test/Lesson6Test.java
@@ -0,0 +1,47 @@
+package ru.bulldog.Lesson6.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import ru.bulldog.Lesson6.main.Lesson6;
+
+import java.util.Arrays;
+
+public class Lesson6Test {
+
+	@Test
+	public void test1_1() {
+		Assert.assertArrayEquals(new int[]{6, 7, 8}, Lesson6.getAllAfterLast4(1, 2, 3, 4, 4, 5, 4, 6, 7, 8));
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void test1_2() {
+		Assert.assertArrayEquals(new int[] { 6, 7, 8 }, Lesson6.getAllAfterLast4(1, 2, 3, 5, 6, 7, 8));
+	}
+
+	@Test
+	public void test1_3() {
+		Assert.assertArrayEquals(new int[] { 1, 3 }, Lesson6.getAllAfterLast4(1, 4, 5, 5, 5, 5, 4, 4, 2, 3));
+	}
+
+	@Test
+	public void test2_1() {
+		Assert.assertTrue(Lesson6.validate1And4(1, 1, 1, 4, 1, 4, 1, 1));
+	}
+
+	@Test
+	public void test2_2() {
+		Assert.assertFalse(Lesson6.validate1And4(1, 1, 1, 4, 4, 4, 5, 1));
+	}
+
+	@Test
+	public void test2_3() {
+		Assert.assertFalse(Lesson6.validate1And4(1, 1, 1, 1, 1, 1, 1, 1));
+	}
+
+	@Test
+	public void test2_4() {
+		Assert.assertFalse(Lesson6.validate1And4(4, 4, 4, 4));
+	}
+}
